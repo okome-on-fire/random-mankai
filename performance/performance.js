bcacfc1b789d9b1573a049583dbaa855cf895a05
komenouka <nekozuki_313@yahoo.co.jp>
2022-05-31 23:44:28 +0900

    ランダム係数の修正

--- performance/performance.js
+++ performance/performance.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
-    var dataJson = $.getJSON('./data.json');
-    dataJson.done(function(dataList) { // jsonの読み込みに成功した時
-        var dataNum = Object.keys(dataList.data).length;
-        console.log(dataNum);
-    })
+    var dataJson = $.getJSON('https://raw.githubusercontent.com/okome-on-fire/random-mankai/main/performance/data.json');
 
     // ランダム選択
     $('.s-choose_button').on('click', function() {
-        var i = 0;
+        dataJson.done(function(dataList) { // jsonの読み込みに成功した時
+            var dataNum = Object.keys(dataList).length;
+            console.log(dataNum);
+
+            let randomNum = Math.floor(Math.random() * dataNum);
+            let resultName = dataList[randomNum].name;
+            let resultPerformance = dataList[randomNum].performance;
+            let resultCharacter = dataList[randomNum].character;
+            $('.s-resultName').text(resultName);
+            $('.s-resultPerformance').text(resultPerformance);
+            $('.s-resultCharacter span').text(resultCharacter);
+
+            // tweet
+            var tweet = '選ばれたのは「' + resultName + '/' + resultPerformance + '」です';
+            $('.s-tweet_button').attr('href', 'http://twitter.com/intent/tweet?url=https://okome-on-fire.github.io/random-mankai/performance/&text=' + tweet + '&hashtags=ランダム公演キャラお題');
+        });
     });
 });
