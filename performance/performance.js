a298d2df2c27421addbf53d25c9e4e80ac8417b1
komenouka <nekozuki_313@yahoo.co.jp>
2022-06-01 01:15:20 +0900

    ランダムの仕組みを変更

--- performance/performance.js
+++ performance/performance.js
@@ -3,14 +3,57 @@ $(document).ready(function() {
 
     // ランダム選択
     $('.s-choose_button').on('click', function() {
+        var troupe = $('.choose_box select').val();
+        console.log(troupe);
         dataJson.done(function(dataList) { // jsonの読み込みに成功した時
-            var dataNum = Object.keys(dataList).length;
-            console.log(dataNum);
+            console.log(dataJson);
+            switch (troupe) {
+                case '全部':
+                    // 全部同じ配列に入れたい
+                    var troupeNum = Math.floor(Math.random() * 2);
+                    switch (troupeNum) {
+                        case 0:
+                            var troupeName = "春組";
+                            break;
+                        case 1:
+                            var troupeName = "夏組";
+                            break;
+                            // case 2:
+                            //     var troupeName = "秋組";
+                            //     break;
+                            // case 3:
+                            //     var troupeName = "冬組";
+                            //     break;
 
-            let randomNum = Math.floor(Math.random() * dataNum);
-            let resultName = dataList[randomNum].name;
-            let resultPerformance = dataList[randomNum].performance;
-            let resultCharacter = dataList[randomNum].character;
+                    }
+                    var dataNum = Object.keys(dataList[0][troupeName]).length;
+                    console.log(dataNum);
+                    var randomNum = Math.floor(Math.random() * dataNum);
+                    var resultName = dataList[0][troupeName][randomNum].name;
+                    var resultPerformance = dataList[0][troupeName][randomNum].performance;
+                    var resultCharacter = dataList[0][troupeName][randomNum].character;
+                    break;
+
+                case '春組':
+                    var dataNum = Object.keys(dataList[0]["春組"]).length;
+                    console.log(dataNum);
+                    var randomNum = Math.floor(Math.random() * dataNum);
+                    var resultName = dataList[0]["春組"][randomNum].name;
+                    var resultPerformance = dataList[0]["春組"][randomNum].performance;
+                    var resultCharacter = dataList[0]["春組"][randomNum].character;
+
+                    break;
+                case '夏組':
+                    var dataNum = Object.keys(dataList[0]["夏組"]).length;
+                    console.log(dataNum);
+                    var randomNum = Math.floor(Math.random() * dataNum);
+                    var resultName = dataList[0]["夏組"][randomNum].name;
+                    var resultPerformance = dataList[0]["夏組"][randomNum].performance;
+                    var resultCharacter = dataList[0]["夏組"][randomNum].character;
+                    break;
+            }
+
+            // 各文字代入
             $('.s-resultName').text(resultName);
             $('.s-resultPerformance').text(resultPerformance);
             $('.s-resultCharacter span').text(resultCharacter);
