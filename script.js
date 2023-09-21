a8d5263bbebde9f89e610311388b57c2d31d6345
komenouka <nekozuki_313@yahoo.co.jp>
2023-09-22 02:08:24 +0900

    団員ランダム表示を複数人対応

--- script.js
+++ script.js
@@ -26,9 +26,67 @@ $(document).ready(function() {
         'ガイ'
     ];
 
+    function rangeRandom(select_num) {
+        // 範囲の最小値
+        var rangeMin = 0;
+        // 範囲の最大値
+        var rangeMax = 23;
+        // 範囲内の数値の個数
+        var rangeLength = rangeMax - rangeMin + 1;
+        // 並び替え前の数値を管理する配列
+        var countArr = [];
+        // 並び替え後の数値を格納する配列
+        var randomArr = [];
+
+        // 範囲内の数値をcountArrに格納
+        for(var i = 0; i < rangeLength; i++) {
+            countArr[i] = i + rangeMin;
+        }
+
+        for(var i = 0; i < select_num; i++) {
+            // 0～countArrの個数 の範囲から、数値をランダムに抽出
+            var randomTarget = Math.floor(Math.random() * countArr.length);
+            // randomArrに数値を格納(randomTargetの数値を格納するのではなく、countArrのrandomTarget番目の配列の数値を格納)
+            randomArr[i] = countArr[randomTarget];
+            // 同じ数値を再度使わないように、今回使った数値をcountArrから削除しておく。
+            countArr.splice(randomTarget, 1);
+        }
+        return randomArr;
+    }
+
+    // 複数人版
+    $('.s-chooseNum_button').on('click', function() {
+        $('.result_item').remove();
+
+        var select_num = $('.selectNum_area input').val();
+        var randomNumList = [];
+
+        randomNumList = rangeRandom(select_num);
+
+        var selectChara = '';
+        var tweetText = '';
+
+        $('.result_title span').text(select_num);
+
+        for(var i = 0; i < select_num; i++) {
+            selectChara = String(i+1) + '：' + nameList[randomNumList[i]];
+            resultText = '<li class="result_item"><p>' + selectChara + '</p></li>' ;
+            $('.s-resultList').append(resultText);
+            if( i == 0) {
+                tweetText += nameList[randomNumList[i]];
+            } else {
+                tweetText += ( '/' + nameList[randomNumList[i]]) ;
+            }
+
+        }
+
+        var tweet = 'ランダムで' + select_num + '人選びました。（' + tweetText + '）';
+        $('.s-tweet_button').attr('href', 'http://twitter.com/intent/tweet?url=https://okome-on-fire.github.io/random-mankai/&text=' + tweet);
+    });
+
+    // 一人用
     $('.s-choose_button').on('click', function() {
         var random = Math.floor(Math.random() * 24);
-        console.log(random);
         $('.s-result').text(nameList[random]);
         var tweet = '選ばれたのは「' + nameList[random] + '」です';
         $('.s-tweet_button').attr('href', 'http://twitter.com/intent/tweet?url=https://okome-on-fire.github.io/random-mankai/&text=' + tweet);
